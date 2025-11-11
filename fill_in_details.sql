c5403a1cb74eea565071741f89a1bffc83797cd6
yopsitsme <chpkats@gmail.com>
2025-11-11 20:44:42 +0200

    upload all files

--- fill_in_details.sql
+++
@@ -1,6 +0,0 @@
-COPY needs (equipment_id,sports_class_id,quantity_required)
-FROM 'C:/Users/User/Desktop/proDB/needs.csv'     -- path to you csv_file
-
--- C:/Users/User/Desktop/תבונה/לימודים/השלמת תואר/שנה ב/סמסטר א/מיניפ בסיסי נתונים/needs.csv
-DELIMITER ','
-CSV HEADER;
